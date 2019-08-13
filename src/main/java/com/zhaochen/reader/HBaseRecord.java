3fba796c7b56524163430b5a680db743ec2fa13e
zhaochen <zhaochen8911@163.com>
2019-08-14 02:18:49 +0800

    修改hbase record

--- src/main/java/com/zhaochen/reader/HBaseRecord.java
+++ src/main/java/com/zhaochen/reader/HBaseRecord.java
@@ -2,18 +2,27 @@ package com.zhaochen.reader;
 
 import com.alibaba.fastjson.JSONObject;
 
+import java.util.List;
+import java.util.Map;
 import java.util.NavigableMap;
 
 public interface HBaseRecord {
+
     NavigableMap<String, String> getInfoMap();
+
+    NavigableMap<String, NavigableMap<Long, String>> getMultipleInfoMap();
 
     JSONObject getInfoJson();
 
-    boolean containsColumn();
+    JSONObject getMultipleInfoJson();
 
-    boolean containsColumns();
+    boolean containsColumn(String column);
+
+    boolean containsColumns(List<String> columns);
 
     String getValue(String column);
+
+    Map<Long, String> getMultipleValue(String column);
 
     String getUid();
 
--- src/main/java/com/zhaochen/reader/HBaseRecordImpl.java
+++ src/main/java/com/zhaochen/reader/HBaseRecordImpl.java
@@ -1,33 +1,86 @@
 package com.zhaochen.reader;
 
 import com.alibaba.fastjson.JSONObject;
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.CellUtil;
+import org.apache.hadoop.hbase.client.Result;
 
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+import java.util.Map;
 import java.util.NavigableMap;
+import java.util.TreeMap;
 
 public class HBaseRecordImpl implements HBaseRecord{
+
+    NavigableMap<String, NavigableMap<Long, String>> resultMap = null;
+
+    public HBaseRecordImpl(Result result) throws UnsupportedEncodingException {
+        this.resultMap = result2Map(result);
+    }
+
+    private NavigableMap<String, NavigableMap<Long, String>> result2Map(Result result) throws UnsupportedEncodingException {
+        NavigableMap<String, NavigableMap<Long, String>> resultMap = new TreeMap<>();
+        for(Cell cell : result.listCells()){
+            String key = new String(CellUtil.cloneQualifier(cell),"utf-8");
+            String value = new String(CellUtil.cloneValue(cell),"utf-8");
+            if (!resultMap.keySet().contains(key)){
+                NavigableMap<Long, String> valueMap = new TreeMap<>();
+                resultMap.put(key, valueMap);
+            }
+            long ts = cell.getTimestamp();
+            resultMap.get(key).put(ts, value);
+        }
+        return resultMap;
+    }
+
     @Override
     public NavigableMap<String, String> getInfoMap() {
-        return null;
+        NavigableMap<String, String> result = new TreeMap<>();
+        for (String key: this.resultMap.keySet()){
+            result.put(key, resultMap.get(key).firstEntry().getValue());
+        }
+        return result;
+    }
+
+    @Override
+    public NavigableMap<String, NavigableMap<Long, String>> getMultipleInfoMap() {
+        return this.resultMap;
     }
 
     @Override
     public JSONObject getInfoJson() {
-        return null;
+        return new JSONObject((TreeMap)getInfoMap());
     }
 
     @Override
-    public boolean containsColumn() {
-        return false;
+    public JSONObject getMultipleInfoJson() {
+        return new JSONObject((TreeMap)this.resultMap);
     }
 
     @Override
-    public boolean containsColumns() {
-        return false;
+    public boolean containsColumn(String column) {
+        return resultMap.keySet().contains(column);
+    }
+
+    @Override
+    public boolean containsColumns(List<String> columns) {
+        for (String column : columns){
+            if (!containsColumn(column)){
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
     public String getValue(String column) {
-        return null;
+        return getInfoMap().get(column);
+    }
+
+    @Override
+    public Map<Long, String> getMultipleValue(String column) {
+        return this.resultMap.get(column);
     }
 
     @Override
